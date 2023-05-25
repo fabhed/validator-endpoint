6235acc88c0e2c47f7a935a016301011b581c75a
Fabian Hedin <fabian@tentium.se>
2023-05-25 17:48:10 +0200

    Re-implemented db layer with peewee

--- btvep/db/__init__.py
+++ btvep/db/__init__.py
@@ -1,1 +1,8 @@
 from . import api_keys, utils, tables
+
+import os
+from peewee import SqliteDatabase
+
+DB_PATH = os.path.join(os.path.dirname(__file__), "../../btvep.db")
+DB_PATH = os.path.abspath(DB_PATH)
+db = SqliteDatabase(DB_PATH)
--- btvep/db/api_keys.py
+++ btvep/db/api_keys.py
@@ -1,6 +1,10 @@
-from tabulate import tabulate
-from .utils import create_or_get_connection
+import datetime
+import json
+from collections import OrderedDict
 
+from tabulate import tabulate
+
+from . import db
 
 column_order = (
     "id",
@@ -15,24 +19,30 @@ column_order = (
     "updated_at",
 )
 
+import secrets
 
-def get_column_to_query(query):
-    return "id" if isinstance(query, int) else "api_key"
+KEY_LENGTH = 48
 
 
-class ApiKey:
-    # Init method taking db output
-    def __init__(self, db_output):
-        self.id = db_output[0]
-        self.api_key = db_output[1]
-        self.api_key_hint = db_output[2]
-        self.name = db_output[3]
-        self.request_count = db_output[4]
-        self.valid_until = db_output[5]
-        self.credits = db_output[6]
-        self.enabled = db_output[7]
-        self.created_at = db_output[8]
-        self.updated_at = db_output[9]
+#################### Reimplment this file with peewee ####################
+from peewee import BooleanField, DateTimeField, IntegerField, Model, TextField
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class ApiKey(BaseModel):
+    api_key = TextField(unique=True)
+    api_key_hint = TextField()
+    name = TextField(null=True)
+    request_count = IntegerField(default=0)
+    valid_until = DateTimeField(default=-1)
+    credits = IntegerField(default=-1)
+    enabled = BooleanField(default=True)
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
 
     def has_unlimited_credits(self):
         return self.credits == -1
@@ -40,110 +50,55 @@ class ApiKey:
     def has_lifetime(self):
         return self.valid_until != -1
 
-    # Define print as a table
     def __str__(self):
-        return tabulate(
-            [self.__dict__.values()],
-            headers=column_order,
-        )
+        # Print dict data with indents using json
+
+        # sortedDict = OrderedDict(
+        #     sorted(self.__dict__["__data__"], key=lambda x: column_order.index(x[0]))
+        # )
+        return json.dumps(self.__dict__["__data__"], indent=4, default=str)
 
     # Define print as a table for lists of ApiKey objects static
     @staticmethod
     def tabulate(api_keys):
         return tabulate(
-            [api_key.__dict__.values() for api_key in api_keys],
-            headers=column_order,
+            api_keys,
+            headers="keys",
         )
-
-
-import secrets
-
-KEY_LENGTH = 48
 
 
 def create_table():
-    connection = create_or_get_connection()
-    with connection:
-        cur = connection.cursor()
-        # Api keys table
-        cur.execute(
-            """
-            CREATE TABLE IF NOT EXISTS api_keys (
-                id INTEGER PRIMARY KEY,
-                api_key TEXT UNIQUE,
-                api_key_hint TEXT,
-                name TEXT,
-                request_count INTEGER,
-                valid_until INTEGER,
-                credits INTEGER,
-                enabled INTEGER,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
-            """
-        )
+    db.create_tables([ApiKey])
 
 
-# Set defaults in params
 def insert(
     api_key: str = secrets.token_urlsafe(KEY_LENGTH),
     request_count: int = 0,
-    # Test
     valid_until: int = -1,
     credits: int = -1,
     enabled: bool = True,
     name: str = None,
 ):
-    connection = create_or_get_connection()
-    cur = connection.cursor()
-
-    with connection:
-        cur.execute(
-            """
-            INSERT INTO api_keys (api_key, api_key_hint, name, request_count, valid_until, credits, enabled)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-            """,
-            (
-                api_key,
-                "..." + api_key[-4:],
-                name,
-                request_count,
-                valid_until,
-                credits,
-                enabled,
-            ),
-        )
-        return get(api_key)
+    return ApiKey.create(
+        api_key=api_key,
+        api_key_hint="..." + api_key[-4:],
+        request_count=request_count,
+        valid_until=valid_until,
+        credits=credits,
+        enabled=enabled,
+        name=name,
+    )
 
 
 def get(query: str | int) -> ApiKey:
     column_to_query = get_column_to_query(query)
-    connection = create_or_get_connection()
-    cur = connection.cursor()
-    cur.execute(
-        """
-        SELECT * FROM api_keys WHERE {} = ?
-        """.format(
-            column_to_query
-        ),
-        (query,),
-    )
-    res = cur.fetchone()
-    return None if res is None else ApiKey(res)
+    return ApiKey.get(column_to_query == query)
 
 
 def get_all():
-    connection = create_or_get_connection()
-    cur = connection.cursor()
-    cur.execute(
-        """
-        SELECT * FROM api_keys
-        """
-    )
-    return [ApiKey(row) for row in cur.fetchall()]
+    return [key for key in ApiKey.select().dicts()]
 
 
-# Update function
 def update(
     query: str | int,
     api_key_hint: str = None,
@@ -153,56 +108,35 @@ def update(
     credits: int = None,
     enabled: bool = None,
 ):
-    """
-    Update an api key with the given query.
-    Args:
-        query: The query to find the api key to update. Can be either the api key or the id.
-
-    """
-    column_to_query = get_column_to_query(query)
-
-    connection = create_or_get_connection()
-    cur = connection.cursor()
-    with connection:
-        cur.execute(
-            """
-            UPDATE api_keys
-            SET api_key_hint = COALESCE(?, api_key_hint),
-                name = COALESCE(?, name),
-                request_count = COALESCE(?, request_count),
-                valid_until = COALESCE(?, valid_until),
-                credits = COALESCE(?, credits),
-                enabled = COALESCE(?, enabled),
-                updated_at = CURRENT_TIMESTAMP
-            WHERE {} = ?
-            """.format(
-                column_to_query
-            ),
-            (
-                api_key_hint,
-                name,
-                request_count,
-                valid_until,
-                credits,
-                enabled,
-                query,
-            ),
-        )
-        return get(query)
+    api_key = get(query)
+    print("api_key", api_key)
+    if api_key_hint is not None:
+        api_key.api_key_hint = api_key_hint
+    if name is not None:
+        api_key.name = name
+    if request_count is not None:
+        api_key.request_count = request_count
+    if valid_until is not None:
+        api_key.valid_until = valid_until
+    if credits is not None:
+        api_key.credits = credits
+    if enabled is not None:
+        api_key.enabled = enabled
+    api_key.save()
+    return api_key
 
 
-# Delete function
 def delete(api_key: str | int):
     column_to_query = get_column_to_query(api_key)
-    connection = create_or_get_connection()
-    cur = connection.cursor()
-    with connection:
-        cur.execute(
-            """
-            DELETE FROM api_keys
-            WHERE {} = ?
-            """.format(
-                column_to_query
-            ),
-            (api_key,),
-        )
+    api_key = get(column_to_query == api_key)
+    a = api_key.delete_instance()
+    print("a", a)
+
+
+def get_column_to_query(query: str | int):
+    if isinstance(query, str):
+        return ApiKey.api_key
+    elif isinstance(query, int):
+        return ApiKey.id
+    else:
+        raise ValueError("Query must be either a string or an integer")

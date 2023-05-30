a27ed58e1707af0af3346a84f4f432b6978f101b
Fabian Hedin <fabian@tentium.se>
2023-05-30 08:35:30 +0200

    Overwrite peewee to use contextvars (for fastapi compatability)

--- btvep/db/utils.py
+++ btvep/db/utils.py
@@ -1,11 +1,31 @@
+from contextvars import ContextVar
 import os
-from peewee import SqliteDatabase, Model
+import peewee
 
 DB_PATH = os.path.join(os.path.dirname(__file__), "../../btvep.db")
 DB_PATH = os.path.abspath(DB_PATH)
-db = SqliteDatabase(DB_PATH, check_same_thread=False)
+
+# https://fastapi.tiangolo.com/advanced/sql-databases-peewee/#make-peewee-async-compatible-peeweeconnectionstate
+db_state_default = {"closed": None, "conn": None, "ctx": None, "transactions": None}
+db_state = ContextVar("db_state", default=db_state_default.copy())
 
 
-class BaseModel(Model):
+class PeeweeConnectionState(peewee._ConnectionState):
+    def __init__(self, **kwargs):
+        super().__setattr__("_state", db_state)
+        super().__init__(**kwargs)
+
+    def __setattr__(self, name, value):
+        self._state.get()[name] = value
+
+    def __getattr__(self, name):
+        return self._state.get()[name]
+
+
+db = peewee.SqliteDatabase(DB_PATH, check_same_thread=False)
+db._state = PeeweeConnectionState()
+
+
+class BaseModel(peewee.Model):
     class Meta:
         database = db

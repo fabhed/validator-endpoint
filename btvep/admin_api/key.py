984bf2e55e36adb93041bc4567f9ecb38cbe4eb5
Fabian Hedin <fabian@tentium.se>
2023-06-17 20:04:13 +0200

    fixed valid_until parsing

--- btvep/admin_api/key.py
+++ btvep/admin_api/key.py
@@ -59,28 +59,29 @@ def edit_api_key(
     """
     Edit an API key.
     """
-    if valid_until is None:
-        parsed_valid_until = None
-    elif valid_until.lower() == "false" or valid_until.lower() == "-1":
-        valid_until = -1
-    else:
-        parsed_valid_until = dateparser.parse(
-            valid_until,
-            settings={
-                "PREFER_DATES_FROM": "future",
-                "DATE_ORDER": "YMD",
-                "PREFER_DAY_OF_MONTH": "first",
-                "RELATIVE_BASE": datetime.now().replace(
-                    hour=0, minute=0, second=0, microsecond=0
-                ),
-            },
-        )
-        if parsed_valid_until is None:
-            raise HTTPException(
-                status_code=400,
-                detail=f"Could not parse date '{valid_until}'. Try another format.",
+    parsed_valid_until = None
+    if valid_until is not None:
+        if valid_until.lower() == "false" or valid_until.lower() == "-1":
+            parsed_valid_until = -1
+        else:
+            parsed_valid_until = dateparser.parse(
+                valid_until,
+                settings={
+                    "PREFER_DATES_FROM": "future",
+                    "DATE_ORDER": "YMD",
+                    "PREFER_DAY_OF_MONTH": "first",
+                    "RELATIVE_BASE": datetime.now().replace(
+                        hour=0, minute=0, second=0, microsecond=0
+                    ),
+                },
             )
 
+            if parsed_valid_until is None:
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Could not parse date '{valid_until}'. Try another format.",
+                )
+            parsed_valid_until = int(parsed_valid_until.timestamp())
     api_keys.update(
         query,
         api_key_hint,

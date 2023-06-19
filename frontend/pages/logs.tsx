40bd4b31b6a7c487fe617f118d806046cc227651
Fabian Hedin <fabian@tentium.se>
2023-06-19 22:56:19 +0200

    Added logs table with client-side search

---
+++ frontend/pages/logs.tsx
@@ -0,0 +1,145 @@
+import { useEffect, useState, useRef } from "react";
+import { Table, Input, Button, Space, InputRef } from "antd";
+import Highlighter from "react-highlight-words";
+import { SearchOutlined } from "@ant-design/icons";
+import useSWR from "swr";
+import fetcher from "../utils/fetcher";
+import { FilterDropdownProps } from "antd/es/table/interface";
+
+export default function Logs() {
+  const { data, error } = useSWR("/admin/logs/", fetcher);
+
+  const [searchText, setSearchText] = useState<React.Key>("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef<InputRef>(null);
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }: FilterDropdownProps) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) => {
+      debugger;
+      return record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .trim()
+        .includes((value || "").toLowerCase().trim());
+    },
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  const logsData = data ? data : [];
+
+  const columns = [
+    {
+      title: "Timestamp",
+      dataIndex: "timestamp",
+      key: "timestamp",
+      sorter: (a, b) =>
+        new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime(),
+      ...getColumnSearchProps("timestamp"),
+    },
+    {
+      title: "API Key Id",
+      dataIndex: "api_key",
+      key: "api_key",
+      ...getColumnSearchProps("api_key"),
+    },
+    {
+      title: "Responder Hotkey",
+      dataIndex: "responder_hotkey",
+      key: "responder_hotkey",
+      ...getColumnSearchProps("responder_hotkey"),
+    },
+  ];
+
+  return (
+    <div>
+      <Table columns={columns} dataSource={logsData} rowKey="timestamp" />
+    </div>
+  );
+}

500faed5224f0e90c31eaf8267a909727363546e
Fabian Hedin <fabian@tentium.se>
2023-06-20 01:25:48 +0200

    added stats comp and count hook

---
+++ frontend/SWR/useCount.tsx
@@ -0,0 +1,33 @@
+import useSWR from "swr";
+import fetcher from "../utils/fetcher";
+
+interface CommonFilters {
+  key?: string;
+  responder_hotkey?: string;
+  start?: number;
+  end?: number;
+  unique_api_keys?: boolean;
+}
+
+export const useCount = (filters: CommonFilters) => {
+  const queryString = Object.keys(filters)
+    .map(
+      (key) =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(
+          filters[key as keyof CommonFilters] || ""
+        )}`
+    )
+    .join("&");
+
+  const {
+    data: countData,
+    error,
+    isLoading,
+  } = useSWR(`/admin/logs/count?${queryString}`, fetcher);
+
+  return {
+    count: countData?.count,
+    isLoading: isLoading,
+    error,
+  };
+};

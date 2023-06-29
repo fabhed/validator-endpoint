47efaed00c22a02f6c6bccd06c6273a35479b6a9
Fabian Hedin <fabian@tentium.se>
2023-06-29 23:38:39 +0200

    made curl example dynamic

--- frontend/pages/api-keys/index.tsx
+++ frontend/pages/api-keys/index.tsx
@@ -404,7 +404,7 @@ export default function ViewApiKeys() {
           <APIKeyOperations
             prompt="What is 1+1?"
             apiKey={record.api_key}
-            url="http://localhost:8000" // TODO: Make this dynamic
+            url={process.env.NEXT_PUBLIC_API_BASE_URL}
             onDelete={() => handleDelete(record)}
           />
         );

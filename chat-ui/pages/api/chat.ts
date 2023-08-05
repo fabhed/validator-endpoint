17509533e14f86f4fd3dd8894d7a42e5f4e6a0f2
Fabian Hedin <fabian@tentium.se>
2023-08-05 21:50:11 +0200

    Hook up backend to validator endpoint instead of OpenAI

--- chat-ui/pages/api/chat.ts
+++ chat-ui/pages/api/chat.ts
@@ -1,63 +1,61 @@
-import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
-import { OpenAIError, OpenAIStream } from '@/utils/server';
+import {
+  DEFAULT_SYSTEM_PROMPT,
+  VALIDATOR_ENDPOINT_BASE_URL,
+} from '@/utils/app/const';
 
 import { ChatBody, Message } from '@/types/chat';
-
-// @ts-expect-error
-import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
-
-import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
-import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
 
 export const config = {
   runtime: 'edge',
 };
 
+export class ValidatorEndpointError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'ValidatorEndpointError';
+  }
+}
+
 const handler = async (req: Request): Promise<Response> => {
   try {
-    const { model, messages, key, prompt, temperature } = (await req.json()) as ChatBody;
+    const { messages, key, prompt } = (await req.json()) as ChatBody;
 
-    await init((imports) => WebAssembly.instantiate(wasm, imports));
-    const encoding = new Tiktoken(
-      tiktokenModel.bpe_ranks,
-      tiktokenModel.special_tokens,
-      tiktokenModel.pat_str,
-    );
-
-    let promptToSend = prompt;
-    if (!promptToSend) {
-      promptToSend = DEFAULT_SYSTEM_PROMPT;
-    }
-
-    let temperatureToUse = temperature;
-    if (temperatureToUse == null) {
-      temperatureToUse = DEFAULT_TEMPERATURE;
-    }
-
-    const prompt_tokens = encoding.encode(promptToSend);
-
-    let tokenCount = prompt_tokens.length;
     let messagesToSend: Message[] = [];
 
     for (let i = messages.length - 1; i >= 0; i--) {
       const message = messages[i];
-      const tokens = encoding.encode(message.content);
-
-      if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
-        break;
-      }
-      tokenCount += tokens.length;
       messagesToSend = [message, ...messagesToSend];
     }
 
-    encoding.free();
+    const res = await fetch(VALIDATOR_ENDPOINT_BASE_URL + '/chat', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${key}`,
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        messages: [
+          {
+            role: 'system',
+            content: prompt || DEFAULT_SYSTEM_PROMPT,
+          },
+          ...messages,
+        ],
+        top_n: 1,
+      }),
+    });
 
-    const stream = await OpenAIStream(model, promptToSend, temperatureToUse, key, messagesToSend);
+    const json = await res.json();
+    console.log(json);
 
-    return new Response(stream);
+    if (res.status !== 200) {
+      throw new ValidatorEndpointError(json?.detail || 'Unknown error');
+    }
+
+    return new Response(json);
   } catch (error) {
     console.error(error);
-    if (error instanceof OpenAIError) {
+    if (error instanceof ValidatorEndpointError) {
       return new Response('Error', { status: 500, statusText: error.message });
     } else {
       return new Response('Error', { status: 500 });

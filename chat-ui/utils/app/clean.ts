92172d402f5a589ae7d191bc9b127feb755a4546
Fabian Hedin <fabian@tentium.se>
2023-08-15 17:13:14 +0200

    Removed unused files & fixed ts errors

--- chat-ui/utils/app/clean.ts
+++ chat-ui/utils/app/clean.ts
@@ -12,26 +12,11 @@ export const cleanSelectedConversation = (conversation: Conversation) => {
 
   let updatedConversation = conversation;
 
-  // check for model on each conversation
-  if (!updatedConversation.model) {
-    updatedConversation = {
-      ...updatedConversation,
-      model: updatedConversation.model || OpenAIModels[OpenAIModelID.GPT_3_5],
-    };
-  }
-
   // check for system prompt on each conversation
   if (!updatedConversation.prompt) {
     updatedConversation = {
       ...updatedConversation,
       prompt: updatedConversation.prompt || DEFAULT_SYSTEM_PROMPT,
-    };
-  }
-
-  if (!updatedConversation.temperature) {
-    updatedConversation = {
-      ...updatedConversation,
-      temperature: updatedConversation.temperature || DEFAULT_TEMPERATURE,
     };
   }
 
@@ -66,16 +51,8 @@ export const cleanConversationHistory = (history: any[]): Conversation[] => {
 
   return history.reduce((acc: any[], conversation) => {
     try {
-      if (!conversation.model) {
-        conversation.model = OpenAIModels[OpenAIModelID.GPT_3_5];
-      }
-
       if (!conversation.prompt) {
         conversation.prompt = DEFAULT_SYSTEM_PROMPT;
-      }
-
-      if (!conversation.temperature) {
-        conversation.temperature = DEFAULT_TEMPERATURE;
       }
 
       if (!conversation.folderId) {

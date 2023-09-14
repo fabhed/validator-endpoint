e79fa7745754d197867cdeed0071f87559c0962f
Fabian Hedin <fabian@tentium.se>
2023-09-14 19:05:36 +0200

    fixed radio button styling

--- chat-ui/components/RadioButton.tsx
+++ chat-ui/components/RadioButton.tsx
@@ -27,7 +27,7 @@ export const RadioButton: React.FC<RadioButtonProps> = ({
         checked={checked}
         onChange={onChange}
         disabled={disabled}
-        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300  dark:bg-gray-700 dark:border-gray-600"
       />
       <label
         htmlFor={id}
